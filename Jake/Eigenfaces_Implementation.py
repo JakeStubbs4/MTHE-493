ac6e9f7ac98c5fa9ff47ec2299546f26818ca739
Jake <15jps1@queensu.ca>
2019-11-10 22:17:54 -0500

    Eigenpairs as a class for sorting

--- Jake/Eigenfaces_Implementation.py
+++ Jake/Eigenfaces_Implementation.py
@@ -18,6 +18,16 @@ class FaceImage:
             self.image_array = image
         self.identity = identity
 
+class EigenPair:
+    eigen_value = None
+    magnitude = None
+    eigen_vector = None
+
+    def __init__(self, eigen_value, eigen_vector):
+        self.eigen_value = eigen_value
+        self.magnitude = abs(eigen_value)
+        self.eigen_vector = eigen_vector
+
 def importDataSet(foldername):
     face_images = []
     identity = 0
@@ -33,13 +43,13 @@ def getAverageFace(face_images):
         face_images_arrays.append(image.image_array)
     return np.mean(face_images_arrays, axis=0)
 
-def getFaceDeviation(face_images, average_face):
+def getFaceDeviations(face_images, average_face):
     face_deviations = []
     for face in face_images:
         face_deviations.append(face.image_array - average_face)
     return face_deviations
 
-def getEigenVectors(face_deviations):
+def covarianceEigenvectors(face_deviations):
     A = np.concatenate(face_deviations, axis=1)
     L = np.dot(np.transpose(A),A)
     return np.linalg.eig(L)
@@ -48,8 +58,18 @@ def main():
     face_images = importDataSet(os.getcwd() + '/Jake/faces_dataset')
     print(len(face_images))
     average_face = getAverageFace(face_images)
-    face_deviations = getFaceDeviation(face_images, average_face)
-    eigen_values, eigen_vectors = getEigenVectors(face_deviations)
-    
+    face_deviations = getFaceDeviations(face_images, average_face)
+    eigen_values, eigen_vectors = covarianceEigenvectors(face_deviations)
+    print("eigenvalues: ")
+    print(eigen_values)
+    print("eigenvectors: ")
+    print(eigen_vectors)
+    '''eigen_pairs = []
+    for i in range(len(eigen_values)):
+        eigen_pairs.append(EigenPair(eigen_values[i], eigen_vectors[i]))
+    eigen_pairs.sort(key=lambda x: x.magnitude, reverse=True)
+    for k in range(20):
+        print(eigen_pairs[k].magnitude)'''
+
 
 main()
